2ae3d4a20a9e2e65513283f11c9f2d4233cada62
rahimbd7 <rahimbd7@gmail.com>
2026-05-10 22:04:07 +0600

    Reinstall the vue again and inititalized with some components,css,utils library

---
+++ frontend/src/components/Common/Navbar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="w-full h-16 bg-[#F9F9F9] text-black flex items-center justify-between px-4">
+    <div class="text-lg font-bold">My E-commerce</div>
+    <nav class="space-x-4">
+      <a href="#" class="hover:text-gray-400">Home</a>
+      <a href="#" class="hover:text-gray-400">Shop</a>
+      <a href="#" class="hover:text-gray-400">Cart</a>
+      <a href="#" class="hover:text-gray-400">Profile</a>
+    </nav>
+   
+</div>
+</template>
+
+<style scoped>
+  
+</style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import "./assets/main.css";
 
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import pinia from "./stores";
+import { defaultConfig, plugin } from "@formkit/vue";
+import PrimeVue from "primevue/config";
+
+const app = createApp(App);
+app.use(plugin, defaultConfig);
+app.use(PrimeVue, {unstyled: true});
+app.use(router);
+app.use(pinia);
+app.mount("#app");
